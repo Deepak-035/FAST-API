def7d7db31f1f0c1cbb85c3d0e7e01ca12bc34f3
Deepak-035 <deepak.23cs035@sode-edu.in>
2026-09-15 22:58:21 +0530

    json file uploaded and make a new connection

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+import json
 
 app= FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+
+    return data
+
+
 
 @app.get("/")
 def hello():
@@ -8,4 +17,9 @@ def hello():
 
 @app.get('/about')
 def message():
-    return {'message':'Heyy this is deepak i am learning fast api'}
+    return {'message':'A fully functional api to manae the patients records'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
